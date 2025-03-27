e958cfbbb012b6901071f55c96b986a0ea6dc98d
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-03-27 12:58:15 +0100

    C v0.1 ACADEMY_Modulo
    Añadido el modulo de Academy

---
+++ academy/models/models.py
@@ -0,0 +1,17 @@
+from odoo import models, fields
+
+class Academy(models.Model):
+    _name = 'academy.academy'
+    _description = 'Academy'
+
+    name = fields.Char(string='Class Title', required=True)
+    begin_date = fields.Datetime(string='Start Date')
+    end_date = fields.Datetime(string='End Date')
+    capacity = fields.Integer(string='Capacity')
+    content_type = fields.Selection([
+        ('theoretical', 'Theoretical'),
+        ('practical', 'Practical'),
+        ('problems', 'Problem Resolution'),
+        ('intensive', 'Intensive')
+    ], string='Type of Content')
+
