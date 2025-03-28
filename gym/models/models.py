fb90ea46588de2bc8044435fe27c5ac4b8103c3f
JulianCabanillas <julian.cabanillaseng@outlook.es>
2025-03-28 10:22:01 +0100

    C v0.2 gym
    Corregido todo lo relacionado con las fallas de este modulo, ahora carga datos de pruebas y gestiona correctamente

--- gym/models/models.py
+++ gym/models/models.py
@@ -4,10 +4,10 @@ class Classes(models.Model):
     _name = 'gym.classes'
     _description = 'Gym Classes'
     
-    name = fields.Char(string="Title", required=True, help="name of the gym")
+    name = fields.Char(string="Title", required=True, help="Gym Zero Haker")
     description = fields.Text()
-    start = fields.Datetime('Starts',required=True, autodate = True)
-    end = fields.Datetime('Ends',required=True, autodate = True)
+    start = fields.Datetime('Starts',required=True, default=fields.Datetime.now)
+    end = fields.Datetime('Ends',required=True, default=fields.Datetime.now)
     capacity = fields.Integer("Capacity")
     activityType = fields.Selection([('dance','Dance'),
                                     ('aerobic','Aerobic'),
